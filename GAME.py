bb651563cb120c225f65f207a2c13778b0f13640
panse <47269934+panse98@users.noreply.github.com>
2021-06-07 15:43:27 +0200

    Add files via upload

---
+++ GAME.py
@@ -0,0 +1,10 @@
+
+class Game:
+    def __init__(self):
+        self.num_cups=6
+        self.player1_cups=[4]*self.num_cups
+        self.player2_cups=[4]*self.num_cups
+        self.mancala_cups=[0,0] #dih l bylem feha#
+        
+
+        
