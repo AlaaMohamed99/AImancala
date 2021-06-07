edab90313da54aea06f2450198d13d8cdc65213e
panse <47269934+panse98@users.noreply.github.com>
2021-06-08 01:55:02 +0200

    Add files via upload

--- GAME.py
+++ GAME.py
@@ -12,10 +12,10 @@ class Game:
 
     def draw(self):
         print("Player 1")
-        print('         ','   5',' ','   4',' ','   3',' ','  2',' ','  1',' ','  0')
-        print('[ ]',' ','('+ str(self.player1_cups[0]) + '  )','('+ str(self.player1_cups[1]) + '     )','( '+ str(self.player1_cups[2]) + '    )','( '+ str(self.player1_cups[3]) + '    )','( '+ str(self.player1_cups[4]) + '    )','( '+ str(self.player1_cups[5]) + '    )',' ','[ ]')
+        print('     ','  5','   ','   4',' ','   3','    ','  2','       ','  1','     ','  0')
+        print('[ ]',' ','(  '+ str(self.player1_cups[0]) + '  )','(  '+ str(self.player1_cups[1]) + '   )','(  '+ str(self.player1_cups[2]) + '   )','(   '+ str(self.player1_cups[3]) + '  )','(  '+ str(self.player1_cups[4]) + '   )','(   '+ str(self.player1_cups[5]) + '  )',' ','[ ]')
         print('[ ]',' ','(  '+ str(self.player2_cups[0]) + '  )','(  '+ str(self.player2_cups[1]) + '   )','(  '+ str(self.player2_cups[2]) + '   )','(   '+ str(self.player2_cups[3]) + '  )','(  '+ str(self.player2_cups[4]) + '   )','(   '+ str(self.player2_cups[5]) + '  )',' ','[ ]')
-        print('         ','   0',' ',' 1',' ',' 2',' ',' 3',' ',' 4',' ',' 5')
+        print('     ','  0','   ','   1',' ','   2','    ','  3','       ','  4','     ','  5')
         print('Player 2')
 
 g=Game()
