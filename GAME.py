7f631489ddb7ecb9c18ba871793fa300aee6bd82
panse <47269934+panse98@users.noreply.github.com>
2021-06-08 01:39:36 +0200

    Add files via upload

--- GAME.py
+++ GAME.py
@@ -5,6 +5,20 @@ class Game:
         self.player1_cups=[4]*self.num_cups
         self.player2_cups=[4]*self.num_cups
         self.mancala_cups=[0,0] #dih l bylem feha#
+        print(self.player1_cups)
+
+
         
+
+    def draw(self):
+        print("zfff")
+        print('         ','   5',' ','   4',' ','   3',' ','  2',' ','  1',' ','  0')
+        print('[ ]',' ','('+  + '  )','(    )','(    )','(    )','(    )','(    )',' ','[ ]')
+        print('[ ]',' ','(    )','(    )','(    )','(    )','(    )','(    )',' ','[ ]')
+        print('         ','   0',' ',' 1',' ',' 2',' ',' 3',' ',' 4',' ',' 5')
+        
+
+g=Game()
+g.draw()
 
         
