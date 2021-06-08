00fb10642493d9d643168df530906940d34d3c6b
panse <47269934+panse98@users.noreply.github.com>
2021-06-08 16:39:39 +0200

    Add files via upload

--- GAME.py
+++ GAME.py
@@ -1,27 +1,13 @@
+
 class Game:
     def __init__(self):
         self.num_cups=6
         self.player1_cups=[4]*self.num_cups
         self.player2_cups=[4]*self.num_cups
-        self.mancala_cups=[0,0] #dih l bylem feha#
-        self.board = [0,4,4,4,4,4,4,0,4,4,4,4,4,4]
-        self.player1_freeturn = False
-        self.player2_freeturn = False
-        print(self.player1_cups)
-
-    def RepeatTurn(self):
- #### da msh lcondition bs m7taga agrb lminimax         
- 
-        if self.player1_freeturn == True:
-            return True
-        elif self.player2_freeturn == True:
-            return True
-        else : return False
-    
-    
+        self.mancala_cups=[[0],[0]] #dih l bylem feha#
+        self.final_list=self.player1_cups+self.mancala_cups[0]+self.player2_cups+self.mancala_cups[1]
         
-        
-
+        print(self.final_list)
 
 
         
